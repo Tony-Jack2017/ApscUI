3cbc7eb8222ba2a61f14e4b5d44206ceec9aca89
Tony-Jack2017 <815378559@qq.com>
2024-08-10 20:27:59 +0800

    ++

---
+++ packages/@apsc-base-react/src/components/Menu/index.tsx
@@ -0,0 +1,17 @@
+import {forwardRef} from "react";
+import classNames from "classnames";
+
+const Menu = forwardRef(() => {
+
+  const classes = classNames([
+    "apsc-menu"
+  ])
+
+  return (
+    <div className={classes}>
+
+    </div>
+  )
+})
+
+export default Menu
